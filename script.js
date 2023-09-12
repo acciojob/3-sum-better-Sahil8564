d0e2e51246d4458ea2efdb16e10316702fc28fc8
Sahil8564 <136605579+Sahil8564@users.noreply.github.com>
2023-09-12 19:48:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function threeSum(arr, target) {
 // write your code here
+
+  n = len(arr)
+  closest_sum = float("inf")
+  for i in range(n):
+    for j in range(i + 1, n):
+      for k in range(j + 1, n):
+        sum = arr[i] + arr[j] + arr[k]
+        difference = abs(sum - target)
+        if difference < difference:
+          closest_sum = sum
+  return closest_sum
   
 }
 
